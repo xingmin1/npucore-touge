f92570ae8bd175f9e03dc649fbcac73343415473
张家文 <xmin@example.org>
2026-09-07 20:03:30 +0800

    System automatically submitted

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1027,7 +1027,7 @@ pub fn sys_getrusage(who: isize, usage: *mut Rusage) -> isize {
 }
 
 pub fn sys_mycall() -> isize {
-    let name: &'static str = "My name is Zhang Jiawen";
+    let name: &'static str = "My name is Zhang Jiawen\n";
     for c in name.chars() {
         console_putchar(c as usize);
     }
